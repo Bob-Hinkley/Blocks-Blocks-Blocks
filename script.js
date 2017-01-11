9a75591be25c06df8f056c624b324c5bacaf5a6e
Bob-Hinkley <bob.hinkley1@gmail.com>
2017-01-11 08:27:57 -0600

    Fixed concatination issue.  All boxes are now random color.

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
 
-  var colors = '#';
+
 
   $('button').on('click', function() {
-      console.log('Click detected, initiating Fun Times');
-    $('body').append('<div class="box" style="background-color: ' + colors + ';"><button class="delete">x</button></div>');
+
+    var colors = '#';
 
     //applies random 6 numbers to the variable 'colors'
-    for (var i =0; i < 6; i++) {
+    for (var i = 0; i < 6; i++) {
       colors += randomNumber(0, 9);
     };
     console.log(colors);
 
+
+      // console.log('Click detected, initiating Fun Times');
+    $('body').append('<div class="box" style="background-color: ' + colors + ';"><button class="delete">x</button></div>');
+
   });
 
   $('body').on('click', '.delete', function() {
-     $(this).closest('.box').remove();  //or .children?
+     $(this).closest('.box').remove();
     console.log('Deleting box');
   });
 
