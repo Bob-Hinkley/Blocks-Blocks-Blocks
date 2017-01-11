197454c8571714fa852c91bab16129dfcfdbb15b
Bob-Hinkley <bob.hinkley1@gmail.com>
2017-01-11 17:03:05 -0600

    Fixed script where clicking on a single box will turn it black.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-
-
   $('button').on('click', function() {
 
     var colors = '#';
@@ -16,12 +14,26 @@ $(document).ready(function() {
       // console.log('Click detected, initiating Fun Times');
     $('body').append('<div class="box" style="background-color: ' + colors + ';"><button class="delete">x</button></div>');
 
+    $('.box').on('click', function() {
+      // console.log('Turning Background Black!');
+      $(this).closest('.box').css('background-color', 'black');
+    });
+
   });
 
+
+
   $('body').on('click', '.delete', function() {
-     $(this).closest('.box').remove();
+    // $(this).closest('.box').animate({
+    //       height: 'toggle'
+    //       width: 'toggle'
+    //     });
+     $(this).closest('.box').remove() ;
     console.log('Deleting box');
   });
+
+
+
 
   function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
